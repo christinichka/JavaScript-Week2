17abc6a0cb58a930a11aa3d7637a1a7d5316337f
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-02-16 16:14:54 -0600

    Create 2AArrayExercises01.js
    2AArrayExercises01.js

---
+++ 2AArrayExercises01.js
@@ -0,0 +1,13 @@
+//Create an array with the following entry: 273.15.
+let firstArray = [273.15];
+// console.log(firstArray);
+
+//Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+console.log(firstArray.push(42));
+console.log(firstArray.push("hello"));
+console.log(firstArray);
+
+
+//Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
+console.log(firstArray.push(false, -4.6, "87"));
+console.log(firstArray)
